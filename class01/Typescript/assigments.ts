21f0e32e165af1b689f01b835318f783b6a8590d
Ayesha Arshad <139641190+coding-with-ayesha@users.noreply.github.com>
2023-07-22 12:37:54 -0700

    Update assigments.ts

--- class01/Typescript/assigments.ts
+++ class01/Typescript/assigments.ts
@@ -1,4 +1,5 @@
-console.log("Welcome to typescript");
+They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
 // Define the types for the items
 type Person = {
     name: string;
@@ -18,7 +19,6 @@ type Person = {
     year: number;
     color: string;
   };
-  
   // Create TypeScript Objects for different items
   const person1: Person = {
     name: "John Doe",
@@ -46,6 +46,7 @@ type Person = {
   
 
 
+  Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 
   function createArrayError() {
     const fruits = ["apple", "orange", "banana"];
@@ -59,6 +60,17 @@ type Person = {
     
 
 
+  Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+  let car = 'subaru';
+  
+  console.log("Is car == 'subaru'? I predict True.")
+  
+  console.log(car == 'subaru')
+  
+  • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+  
+  • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
 
   let car = 'subaru';
 
@@ -93,7 +105,18 @@ console.log("Is car.length > 5? I predict False.");
 console.log(car.length > 5); // Prediction: False (since the length of 'subaru' is not greater than 5)
 
 
+More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+• Tests for equality and inequality with strings
 
+• Tests using the lower case function
+
+• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+• Tests using "and" and "or" operators
+
+• Test whether an item is in a array
+
+• Test whether an item is not in a array
 
 // Tests for equality and inequality with strings
 let language = 'JavaScript';
@@ -152,7 +175,10 @@ console.log(fruits.includes('apple')); // Prediction: True ('apple' is present i
 console.log("Is 'grape' not in the fruits array? I predict True.");
 console.log(!fruits.includes('grape')); // Prediction: True ('grape' is not present in the fruits array)
 
+Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 
 let alien_color = 'green';
 
@@ -161,6 +187,18 @@ if (alien_color === 'green') {
 }
 
 
+Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+
+• If the person is age 65 or older, print a message that the person is an elder.
 
 let age = 30;
 
@@ -179,7 +217,10 @@ if (age < 2) {
 }
 
 
+Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
 
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
 // Make an array of your three favorite fruits
 let favorite_fruits = ['banana', 'strawberry', 'mango'];
 
@@ -205,6 +246,10 @@ if (favorite_fruits.includes('kiwi')) {
 }
 
 
+Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+
+• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
 let usernames = ['admin', 'Eric', 'John', 'Alice', 'Jane'];
 
@@ -217,6 +262,14 @@ for (let username of usernames) {
 }
 
 
+Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
 
 // Make a list of current usernames
 let current_users = ['john', 'mary', 'jane', 'jim', 'susan'];
@@ -240,6 +293,12 @@ for (let new_username of new_users) {
 }
 
 
+Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a array.
+
+• Loop through the array.
+
+• Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -260,6 +319,10 @@ for (let number of numbers) {
 }
 
 
+Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+• Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+• Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
 
 let favorite_pizzas = ['pepperoni', 'margherita', 'hawaiian'];
 
@@ -275,6 +338,7 @@ console.log("I really love pizza!");
 
 
 
+Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
 
 let animals = ['dog', 'cat', 'rabbit'];
 let commonCharacteristic = 'They are all friendly and domesticated animals.';
